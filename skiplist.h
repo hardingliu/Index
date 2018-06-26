5c7dae3cadbb67cb83e3a7fba82606689bc3d624
Zongming Liu <zongmingxliu@gmail.com>
2018-06-26 02:47:23 -0300

    fix format

--- skiplist.h
+++ skiplist.h
@@ -1,25 +1,24 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <cctype>
-#include <sstream>
-#include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 const int MAX_HEIGHT = 16;
 
-struct Node{
+struct Node {
     string entry;
     int counts;
     vector<int> pages;
     int height;
-    Node* next[MAX_HEIGHT];
+    Node *next[MAX_HEIGHT];
 };
 
-
 int randBit(void);
-Node* find(string word, Node* head);
-void insert(string word, int pagei, Node* head);
-void printInOrder(ostream& output, Node* head);
+Node *find(string word, Node *head);
+void insert(string word, int pagei, Node *head);
+void printInOrder(ostream &output, Node *head);
